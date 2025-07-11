c2b475ea8f035e099549cb2058e576df72af826f
edunwant42 <edunwant42@gmail.com>
2025-07-11 15:20:38 +0100

    Fix Task 2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
+// import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
 
