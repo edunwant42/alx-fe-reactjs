59ab189b7d316fffbfb3223fd283bf989e81d37f
edunwant42 <edunwant42@gmail.com>
2025-08-09 14:49:09 +0100

    Task 0: Setting Up Tailwind CSS in a New React Project

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-500 underline hover:scale-105">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
